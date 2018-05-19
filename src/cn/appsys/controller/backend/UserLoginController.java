4d4097d9648f9ee087d1aabe4829604aa5897dcf
daiduyu <anyway_yu@163.com>
2018-05-19 11:35:40 +0800

    init

---
+++ src/cn/appsys/controller/backend/UserLoginController.java
@@ -0,0 +1,57 @@
+package cn.appsys.controller.backend;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import cn.appsys.pojo.BackendUser;
+import cn.appsys.service.backend.BackendUserService;
+import cn.appsys.tools.Constants;
+
+@Controller
+@RequestMapping("/manager")
+public class UserLoginController {
+	@Resource
+	BackendUserService backendUserService;
+	
+	@RequestMapping("/login")
+	public String login() {
+		return "backendlogin";
+	}
+	
+	@RequestMapping("/dologin")
+	public String dologin(@RequestParam String userCode,@RequestParam String userPassword,HttpServletRequest request,HttpSession session) {
+		BackendUser backendUser = null;
+		try {
+			backendUser = backendUserService.login(userCode, userPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		//用户登陆
+		if(backendUser != null) {
+			session.setAttribute(Constants.USER_SESSION, backendUser);
+			return "redirect:/manager/backend/main";
+		}else {
+			request.setAttribute("error", "用户名或密码不正确");
+			return "backendlogin";
+		}
+	}
+	
+	@RequestMapping("/backend/main")
+	public String main(HttpSession session) {
+		if(session.getAttribute(Constants.USER_SESSION) == null) {
+			return "redirect:/manager/login";
+		}
+		return "backend/main";
+	}
+	
+	@RequestMapping("/logout")
+	public String lonout(HttpSession session) {
+		session.removeAttribute(Constants.USER_SESSION);
+		return "backendlogin";
+	}
+	
+}
---
+++ src/cn/appsys/controller/developer/DevLoginController.java
@@ -0,0 +1,68 @@
+package cn.appsys.controller.developer;
+
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import cn.appsys.pojo.DevUser;
+import cn.appsys.service.developer.DevUserService;
+import cn.appsys.tools.Constants;
+
+@Controller
+@RequestMapping("/devuser")
+public class DevLoginController {
+/*	private Logger logger = Logger.getLogger(DevLoginController.class);*/
+
+	@Resource
+	DevUserService devUserService;
+
+	@RequestMapping("/login")
+	public String login() {
+		return "devlogin";
+	}
+
+	@RequestMapping("/dologin")
+	public String dologin(@RequestParam String devCode,@RequestParam String devPassword,HttpServletRequest request,HttpSession session) {
+		DevUser devUser = null;
+		try {
+			devUser = devUserService.login(devCode, devPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if(devUser != null) {//登陆成功
+			session.setAttribute(Constants.DEV_USER_SESSION, devUser);
+			return "redirect:/devuser/main";
+		}else {
+			//页面跳转（login.jsp）带出提示信息--转发
+			request.setAttribute("erroor", "用户名或密码不正确");
+			return "devlogin";
+		}
+	}
+
+	@RequestMapping("/main")
+	public String mian(HttpSession session) {
+		if(session.getAttribute(Constants.DEV_USER_SESSION) == null) {
+			return "redirect:/devuser/login";
+		}
+		return "developer/main";
+	}
+
+	/**
+	 * 退出登陆
+	 * @param session
+	 * @return
+	 */
+	@RequestMapping("/logout")
+	public String loginout(HttpSession session) {
+		//清除session
+		session.removeAttribute(Constants.DEV_USER_SESSION);
+		return "devlogin";
+	}
+	
+	
+}
+
